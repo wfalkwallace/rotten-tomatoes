099e84daf988bc55244dd6bae752407bb48a8a14
William Falk-Wallace <wgf2104@columbia.edu>
2015-02-03 22:49:51 -0800

    adds mrprogress for hud progress status

--- Podfile
+++ Podfile
@@ -1,2 +1,3 @@
 platform :ios, '8.0'
 pod 'AFNetworking', '~> 2.5.0'
+pod 'MRProgress', '~> 0.8.1'
--- rottentomatoes/objc_bridging_header.h
+++ rottentomatoes/objc_bridging_header.h
@@ -10,5 +10,6 @@
 #define rottentomatoes_objc_bridging_header_h
 
 #import <AFNetworking/AFNetworking.h>
+#import <MRProgress/MRProgress.h>
 
 #endif
