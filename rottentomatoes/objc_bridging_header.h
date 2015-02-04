828df3526713c2472d88c375375e2e913d93673a
William Falk-Wallace <wgf2104@columbia.edu>
2015-02-03 22:23:19 -0800

    adds bridging header for obj c pods

---
+++ rottentomatoes/objc_bridging_header.h
@@ -0,0 +1,13 @@
+//
+//  objc_bridging_header.h
+//  rottentomatoes
+//
+//  Created by William Falk-Wallace on 2/3/15.
+//  Copyright (c) 2015 Falk-Wallace. All rights reserved.
+//
+
+#ifndef rottentomatoes_objc_bridging_header_h
+#define rottentomatoes_objc_bridging_header_h
+
+
+#endif
