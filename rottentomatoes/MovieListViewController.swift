56f5180e952677574b3352568d5237d4e18d25ab
William Falk-Wallace <wgf2104@columbia.edu>
2015-02-04 22:59:59 -0800

    adds networking call and reorgs for new subclass

--- rottentomatoes/MovieListViewController.swift
+++ rottentomatoes/MovieListViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class MovieListViewController: UIViewController {
+class MovieListViewController: UITableViewController {
 
+    var movies = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let ApiKey = "6t24p36sagyc9xm6mt6ez6xf"
+        let RottenTomatoesURLString = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=\(ApiKey)"
+        let request = NSURLRequest(URL: NSURL(string: RottenTomatoesURLString)!)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response, data, error) in
+            var errorValue: NSError? = nil
+            let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &errorValue) as NSDictionary
+            self.movies = dictionary["movies"] as NSArray
+            self.tableView.reloadData()
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +32,18 @@ class MovieListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("com.falk-wallace.moviecell") as UITableViewCell
+        cell.textLabel?.text = "Row: \(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("Did tap row: \(indexPath.row)")
+    }
 
 }
--- rottentomatoes/ViewController.swift
+++ rottentomatoes/ViewController.swift
@@ -20,18 +20,5 @@ class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("com.falk-wallace.moviecell") as UITableViewCell
-        cell.textLabel?.text = "Row: \(indexPath.row)"
-        return cell
-    }
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        println("Did tap row: \(indexPath.row)")
-    }
 }
 
